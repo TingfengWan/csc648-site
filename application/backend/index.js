925898b1c78231a282e12a0106f9a91a9204a005
John Joshua Gutierrez <johnjoshuagutierrez98@gmail.com>
2020-02-17 18:59:49 -0800

    changed express general endpoint folder directory

--- application/backend/index.js
+++ application/backend/index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 // app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index/index.html'));
 });
 
 app.listen(3000);
