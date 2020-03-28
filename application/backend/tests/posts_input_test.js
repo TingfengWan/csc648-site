e9db71d1e2ae11bdc9337a5495877c3f4488b421
Eric Ngo <ngoaieric@gmail.com>
2020-03-28 13:23:14 -0700

    Blob input.

--- application/backend/tests/posts_input_test.js
+++ application/backend/tests/posts_input_test.js
@@ -1,4 +1,13 @@
 const mysql = require('mysql');
+const fs = require('fs');
+
+function readImageFile(file) {
+// read binary data from a file:
+    const bitmap = fs.readFileSync(file);
+    const buf = new Buffer(bitmap);
+    return buf;
+}
+
 
 // create database connections
 const database = mysql.createConnection({
@@ -8,41 +17,42 @@ const database = mysql.createConnection({
     database: process.env.db_name
 });
 database.connect((err) => {
+    const data = readImageFile('../../assets/default.png');
     if (err) throw err;
     console.log('Connected to test database');
     database.query(`USE ${process.env.db_name}`);
     database.query('INSERT INTO Posts(creator_email,title,media_preview,media_content,file_name,has_file,approver_email,post_body,is_approved) VALUES (\
         student_test@sfsu.edu,\
         "Rubber Ducky",\
-        LOAD_FILE("../../assets/default.png"),\
-        LOAD_FILE("../../assets/default.png"),\
+        BINARY(:data),\
+        BINARY(:data),\
         "RubberDucky.png",\
         true,\
         "admin_faculty_test@sfsu.edu",\
         "A bluber ducky.",\
         true\
-    )');
+    )', {data});
     database.query('INSERT INTO Posts(creator_email,title,media_preview,media_content,file_name,has_file,approver_email,post_body,is_approved) VALUES (\
         student_test@sfsu.edu,\
         "Science floating thingy",\
-        LOAD_FILE("../../assets/default.png"),\
-        LOAD_FILE("../../assets/default.png"),\
+        BINARY(:data),\
+        BINARY(:data),\
         "RubberDucky.png",\
         true,\
         "admin_faculty_test@sfsu.edu",\
         "A floating device.",\
         true\
-    )');
+    )', {data});
     database.query('INSERT INTO Posts(creator_email,title,media_preview,media_content,file_name,has_file,cost,approver_email,post_body,is_approved) VALUES (\
         student_test@sfsu.edu,\
         "Van Gough Duck",\
-        LOAD_FILE("../../assets/default.png"),\
-        LOAD_FILE("../../assets/default.png"),\
+        BINARY(:data),\
+        BINARY(:data),\
         "RubberDucky.png",\
         true,\
         15.0,\
         "admin_faculty_test@sfsu.edu",\
         "An artistic duck.",\
         true\
-    )');
+    )', {data});
 });
