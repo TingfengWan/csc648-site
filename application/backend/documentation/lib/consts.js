d6d3e2629e593d08e94abcd2bde1f039538d4768
Eric Ngo <ngoaieric@gmail.com>
2020-03-28 11:54:04 -0700

    Add db input test. Add post-server search functionality.

---
+++ application/backend/documentation/lib/consts.js
@@ -0,0 +1,16 @@
+// constant file to hold consts in backend
+module.exports = {
+    postServerPort: 4000,
+    sanitizer: (str) => {
+        const map = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#x27;',
+            "/": '&#x2F;',
+        };
+        const reg = /[&<>"'/]/ig;
+        return string.replace(reg, (match)=>(map[match]));
+    }
+}
