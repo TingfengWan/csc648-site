6b2a61425c0a6be0700fdf68cd650d4f1acc4dd2
Ubuntu <ubuntu@ip-172-31-17-36.us-east-2.compute.internal>
2020-03-31 01:39:41 +0000

    changing express directory

--- application/backend/servers/static-server.js
+++ application/backend/servers/static-server.js
@@ -4,7 +4,7 @@ const { staticServerPort } = require('../documentation/lib/consts.js');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../../frontend')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index/index.html'));
