e7303b1d25bc197b9516002cd1c5f0b031345c89
Marvin <marvin.nguy@gmail.com>
2020-03-31 19:40:35 -0700

    search results changes

--- application/frontend/test/search.js
+++ application/frontend/test/search.js
@@ -1,4 +1,8 @@
 
+/**
+ * fetches data for the search results
+ * from the database and displays it
+ */
 function search() {
     var request = new XMLHttpRequest();
     var URL = 'http://3.22.78.154:3000/post/search?';
@@ -31,15 +35,31 @@ function search() {
 
 }
 
+/**
+ * displays the data
+ * from the server into HTML
+ */
 function appendData(data) {
 
     var mainContainer = document.getElementById("search-results");
 
     for(var i = 0; i < data.posts.length; i++) {
-        console.log(data.posts[i].title);
-        var div = document.createElement("div");
-        div.innerHTML = 'Title: ' + data.posts[i].title;
-        mainContainer.appendChild(div);
+
+        var post = data.posts[i];
+        var divTitle = document.createElement('div');      
+        var divDesc = document.createElement('div');
+        const preview = document.createElement('img');
+
+        preview.src = 'data:image/png;base64,' + data.test;
+
+        divTitle.innerHTML = '<h1> Title: ' + post.title + '</h1>';
+        divDesc.innerHTML = '<p> Description: ' + post.post_body + '</p>' +
+                            '<p> Cost: $' + post.cost + '</p>';
+
+  
+        mainContainer.appendChild(divTitle);
+        mainContainer.appendChild(preview);
+        mainContainer.appendChild(divDesc);
     }
 
 }
