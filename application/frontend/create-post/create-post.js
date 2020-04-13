744f64ec71d4a53717e7ada0085e6835e371e629
Marvin <marvin.nguy@gmail.com>
2020-04-12 21:50:15 -0700

    added image preview to create post page

--- application/frontend/create-post/create-post.js
+++ application/frontend/create-post/create-post.js
@@ -9,3 +9,19 @@ function displayUploadFileEle() {
         uploadFileEle.style.display = "";
     }
 }
+
+function displayImagePreview() {
+    var imgPreview = document.getElementById("image-preview");
+    var file = document.getElementById("image").files[0];
+    
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function() {
+        imgPreview.src = reader.result;
+
+        });
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+
+}
