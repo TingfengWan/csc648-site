55a92fa164007b2d2ae1437c6b24fa1385639a92
Marvin <marvin.nguy@gmail.com>
2020-04-13 17:06:48 -0700

    form validation code changes

--- application/frontend/register/register.js
+++ application/frontend/register/register.js
@@ -1,88 +1,66 @@
 function validateFields() {
 
-    var firstNameField = "first-name-field";
-    var lastNameField = "last-name-field";
-    var passwordField = "password-field";
-    var confirmPasswordField = "confirm-password-field";
-    var fields = document.getElementById("register-form").elements;
-    var valid = true;
+    const firstNameField = "first-name-field";
+    const lastNameField = "last-name-field";
+    const passwordField = "password-field";
+    const confirmPasswordField = "confirm-password-field";
+    let fields = document.getElementById("register-form").elements;
+    let valid = true;
 
-    for (var i = 0; i < fields.length; i++) {
-        var field = fields[i];
+    for (let i = 0; i < fields.length; i++) {
+        let field = fields[i];
+        let errorMsg = "";
+        field.style.border = "";
 
-        if(field.type == "text") {
+        if (field.type == "text") {
+            if (field.value == "") {
+                errorMsg = "Please enter a " + field.name + ".";
+                field.style.border = "1px solid red";
+                valid = false;
+            }
+            
             if (field.id == firstNameField) {
-                if (field.value == "") {
-                    field.style.border = "1px solid red";
-                    document.getElementById("first-name-error").innerHTML = "Please enter a first name.";
-                    valid = false;
-                }
-                else {
-                    field.style.border = "";
-                    document.getElementById("first-name-error").innerHTML = "";
-                }
+                document.getElementById("first-name-error").innerHTML = errorMsg;
             }
-    
-            else if (field.id == lastNameField) {
-                if (field.value == "") {
-                    field.style.border = "1px solid red";
-                    document.getElementById("last-name-error").innerHTML = "Please enter a last name.";
-                    valid = false;
-                }
-                else {
-                    field.style.border = "";
-                    document.getElementById("last-name-error").innerHTML = "";
-                }
+            if (field.id == lastNameField) {
+                document.getElementById("last-name-error").innerHTML = errorMsg;
             }
-
         }
 
         else if (field.type == "email") {
             if (!field.value.includes("@sfsu.edu") && !field.value.includes("@mail.sfsu.edu")) {
                 field.style.border = "1px solid red";
-                document.getElementById("email-error").innerHTML = "Please enter a valid email.";
+                errorMsg = "Please enter a valid " + field.name + ".";
                 valid = false;
             }
 
-            else {
-                field.style.border = "";
-                document.getElementById("email-error").innerHTML = "";
-            }
+            document.getElementById("email-error").innerHTML = errorMsg;
         }
 
         else if (field.type == "password") {
             if (fields[passwordField].value != fields[confirmPasswordField].value) {
                 field.style.border = "1px solid red";
-                document.getElementById("password-error").innerHTML = "Passwords do not match.";
+                errorMsg = "Passwords do not match.";
                 valid = false;
             }
-
             else if (field.value == "") {
                 field.style.border = "1px solid red";
-                document.getElementById("password-error").innerHTML = "Please enter a password.";
+                errorMsg = "Please enter a password.";
                 valid = false;
             }
-
-            else {
-                field.style.border = "";
-                document.getElementById("password-error").innerHTML = "";
-            }
+            
+            document.getElementById("password-error").innerHTML = errorMsg;
         }
 
         else if (field.type == "checkbox") {
-            if(field.checked == false) {
+            if (field.checked == false) {
                 field.style.border = "1px solid red";
-                document.getElementById("terms-and-cond-error").innerHTML = "Please accept terms and conditions";
+                errorMsg = "Please accept " + field.name + ".";
                 valid = false;
             }
 
-            else {
-                field.style.border = "";
-                document.getElementById("terms-and-cond-error").innerHTML = "";
-            }
-
+            document.getElementById("terms-and-cond-error").innerHTML = errorMsg;
         }
     }
-
     return valid;
 }
