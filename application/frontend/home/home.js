754ecc92f28f45730379c20d781f310e7b81789e
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-04-18 19:33:53 -0700

    login function and css is done

---
+++ application/frontend/home/home.js
@@ -0,0 +1,25 @@
+function getCategories() {
+    let URL = "http://3.22.78.154:3000/post/categories";
+
+    fetch(
+        URL
+      )
+        .then(data => {
+            return data.json();
+        })
+        .then(function (data) {
+            const categories = data.categories;
+            const categoriesSelect = document.getElementById("category-field");
+
+            for(let i = 0; i < categories.length; i++) {
+                let node = document.createElement("option");
+                node.value = categories[i].category;
+                let text = document.createTextNode(categories[i].category);
+                node.appendChild(text);
+                categoriesSelect.appendChild(node);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
