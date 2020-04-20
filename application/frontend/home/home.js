c085a3b6be2cdc08e8493e178ecb3799eba434be
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-04-19 23:52:15 -0700

    using wrong link to jump from homepage to search

--- application/frontend/home/home.js
+++ application/frontend/home/home.js
@@ -36,7 +36,7 @@ function redirectToSearchResults() {
     event.preventDefault();
     var category = document.getElementById('category-field').value;
     var userInput = document.getElementById('userInput').value;
-    //var url = 'http://3.22.78.154:3000/search/search.html?title=' + userInput + '&category=' + category;
-    var url = 'file:///C:/Users/bubbl/OneDrive/Desktop/School/CSC%20648/csc648-fa20-team03/application/frontend/search/search.html?title=' + userInput + '&category=' + category;
+    var url = 'http://3.22.78.154:3000/search/search.html?title=' + userInput + '&category=' + category;
+    //var url = 'file:///C:/Users/bubbl/OneDrive/Desktop/School/CSC%20648/csc648-fa20-team03/application/frontend/search/search.html?title=' + userInput + '&category=' + category;
     document.location.href =  url;
 }
