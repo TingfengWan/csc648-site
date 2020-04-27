66a44144d6f9052724165a5c08a1321f6517cb4c
Eric Ngo <ngoaieric@gmail.com>
2020-04-27 11:48:27 -0700

    Syntax errors.

--- application/backend/documentation/lib/consts.js
+++ application/backend/documentation/lib/consts.js
@@ -22,28 +22,30 @@ module.exports = {
         return str.replace(reg, (match)=>(map[match]));
     },
     postMapper: (result) => {
-        let locations = [];
-        let categories = [];
-        if ( post.locations ) {
-            locations = post.locations.split(", ")
-        }
-        if ( post.categories ) {
-            categories = post.categories.split(", ");
-        }
-        return result.map(post => ({
-            id: post.id,
-            creator_email: post.creator_email,
-            title: post.title,
-	        create_time: post.create_time,
-            file_name: post.file_name,
-            has_file: post.has_file,
-            cost: post.cost,
-            approver_email: post.approver_email,
-            post_body: post.post_body,
-            is_approved: post.is_approved,
-            media_preview: post.media_preview,
-            locations: locations,
-            categories: categories
-        }));
+        return result.map(post => {
+            let locations = [];
+            let categories = [];
+            if ( post.locations ) {
+                locations = post.locations.split(", ")
+            }
+            if ( post.categories ) {
+                categories = post.categories.split(", ");
+            }
+            return {
+                id: post.id,
+                creator_email: post.creator_email,
+                title: post.title,
+                create_time: post.create_time,
+                file_name: post.file_name,
+                has_file: post.has_file,
+                cost: post.cost,
+                approver_email: post.approver_email,
+                post_body: post.post_body,
+                is_approved: post.is_approved,
+                media_preview: post.media_preview,
+                locations: locations,
+                categories: categories
+            };
+        });
     }
 }
