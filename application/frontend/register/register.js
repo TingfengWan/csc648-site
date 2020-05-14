d7870da25bb58d88d07375945064c4de89c179ae
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-05-14 00:42:31 -0700

    still trying this recaptcha with axios method instead of fetch part 10

--- application/frontend/register/register.js
+++ application/frontend/register/register.js
@@ -22,24 +22,30 @@ function validateForm() {
 
     //sends captcha response to google to verify that it's correct
     axios.post('http://3.22.78.154:3000/user/authenticate', {
-      headers: { "Accept": "application/json, text/plain, */*", "Content-Type": "application/json"},
-    }, captcha)
-    .then((res) => {
-      console.log(res.data);
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+    },
+      {
+        'captcha': captcha
+      })
+      .then((res) => {
+        console.log(res.data);
 
-      if (res.data.success) {
-        document.getElementById("recaptcha-error").innerHTML = "";
-        createUser();
-      }
+        if (res.data.success) {
+          document.getElementById("recaptcha-error").innerHTML = "";
+          createUser();
+        }
 
-      else {
-        document.getElementById("recaptcha-error").innerHTML = "Incorrect recaptcha";
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      document.getElementById("recaptcha-error").innerHTML = "Please verify that you are a human";
-    });
+        else {
+          document.getElementById("recaptcha-error").innerHTML = "Incorrect recaptcha";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        document.getElementById("recaptcha-error").innerHTML = "Please verify that you are a human";
+      });
     // fetch("http://3.22.78.154:3000/user/authenticate", {
     //   method: "POST",
     //   headers: { "Accept": "application/json, text/plain, */*", "Content-Type": "application/json"},
@@ -121,7 +127,7 @@ function validateFields() {
     let errorMsg = "";                        //removes error msg if exists
     field.style.border = "";                  //removes error border if there is any
 
-    if(field.id == "g-recaptcha-response" || field.id == "captcha") {
+    if (field.id == "g-recaptcha-response" || field.id == "captcha") {
       continue;
     }
 
