0a9576ab59574f9e2330324dff8469128a462c5a
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-05-14 00:52:29 -0700

    Revert "Use querystring to stringify json request to captcha."
    This reverts commit 37af395698f741751e7e5b30e7968d84d521e058.

--- application/backend/servers/user-server.js
+++ application/backend/servers/user-server.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mysql = require('mysql');
 const fetch = require('node-fetch');
-const { stringify } = require('querystring');
 
 
 const {userServerPort, sanitizer, postMapper} = require('../documentation/lib/consts.js');
@@ -306,7 +305,7 @@ app.post('/user/authenticate', (req, res) => {
     const secretKey = '6Le6Qe8UAAAAAGqkMmm24EEU2MyPIbqlUUx4fttK';
 
     // Verify URL
-    const query = stringify({
+    const query = JSON.stringify({
         secret: secretKey,
         response: req.body.captcha
     });
