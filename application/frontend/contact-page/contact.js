abacefd496e0fe8e916fa321eab6337fe7251638
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-05-16 17:32:35 -0700

    trying to fix css on contant page

--- application/frontend/contact-page/contact.js
+++ application/frontend/contact-page/contact.js
@@ -42,10 +42,55 @@ function getMessages() {
 
 
 function displayInbox(data) {
-    
+
+    for(let i=0; i < data.messages.length; i++) {
+
+        let inbox = document.getElementById('inboxDiv');
+        let container = document.createElement('div');
+        let email = document.createElement('div');
+        let message = document.createElement('div');
+        let date = document.createElement('div');
+        
+        container.classList.add('container-fluid');
+        container.classList.add('row');
+        container.classList.add('border');
+        container.classList.add('border-secondary');
+        email.classList.add('col');
+        message.classList.add('col');
+        date.classList.add('col');
+        
+        
+        if(data.messages[i].recipient_email == "test@sfsu.edu") {
+            email.innerHTML = `${data.messages[i].sender_email}`;
+            message.innerHTML = `${data.messages[i].message}`;
+            date.innerHTML = `${data.messages[i].timestamp}`;
+        }
+        console.log(date);
+
+        inbox.appendChild(container);
+        container.appendChild(email);
+        container.appendChild(message);
+        container.appendChild(date);
+    }
 }
 
 function displaySent(data) {
 
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
